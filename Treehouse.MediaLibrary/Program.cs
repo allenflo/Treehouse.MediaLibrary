acaba3b8b30072f4fa1e3bd074ca2c1ff9d1f065
allenflo <68171417+allenflo@users.noreply.github.com>
2021-09-13 14:38:07 -0700

    Added Field Value

--- Treehouse.MediaLibrary/Program.cs
+++ Treehouse.MediaLibrary/Program.cs
@@ -7,8 +7,22 @@ namespace Treehouse.MediaLibrary
         static void Main(string[] args)
         {
             var album = new Album();
+            album.Title = "Yellow Submarine";
+            album.Artist = "The Beatles";
+
             var book = new Book();
+            book.Title = "Moby-Dick";
+            book.Author = "Herman Melville";
+
             var movie = new Movie();
+            movie.Title = "Lawrence of Arabia";
+            movie.Director = "David Lean";
+
+            Console.WriteLine("Album: " + album.Title + " by " + album.Artist);
+            Console.WriteLine("Book: " + book.Title + " by " + book.Author);
+            Console.WriteLine("Movie: " + movie.Title + " by " + movie.Director);
         }
+
+
     }
 }
